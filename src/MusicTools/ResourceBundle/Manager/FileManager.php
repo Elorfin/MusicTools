1fc0568455ba38839d3986af5d135f409068b73a
Elorfin <penin.axel@gmail.com>
2015-07-14 10:55:07 +0200

    code refactoring

--- src/MusicTools/ResourceBundle/Manager/FileManager.php
+++ src/MusicTools/ResourceBundle/Manager/FileManager.php
@@ -2,22 +2,56 @@
 
 namespace MusicTools\ResourceBundle\Manager;
 
+use MusicTools\ResourceBundle\Entity\File;
+
+/**
+ * Manages files
+ */
 class FileManager
 {
+    /**
+     * Directory where are stored the uploaded files
+     * @var string
+     */
     protected $uploadDir;
 
+    /**
+     * @param string $uploadDir
+     */
     public function __construct($uploadDir)
     {
         $this->uploadDir = $uploadDir;
     }
 
-    public function upload()
+    /**
+     * Upload a new file
+     * @param \MusicTools\ResourceBundle\Entity\File $file
+     */
+    public function upload(File $file)
     {
+        $fileToUpload = $file->getFile();
+        if (null !== $fileToUpload) {
+            // Generate file name
+            $name = sha1(uniqid(mt_rand(), true)) . '.' . $fileToUpload->guessExtension();
+            $file->setPath($name);
 
+            // Move to upload dir
+            $fileToUpload->move($this->uploadDir, $fileToUpload->getPath());
+
+            // Remove UploadedFile instance
+            $file->setFile(null);
+        }
     }
 
-    public function remove()
+    /**
+     * Remove a file
+     * @param \MusicTools\ResourceBundle\Entity\File $file
+     */
+    public function remove(File $file)
     {
-
+        if (null !== $file->getPath()) {
+            $fullPath = $this->uploadDir . DIRECTORY_SEPARATOR . $file->getPath();
+            unlink($fullPath);
+        }
     }
 }

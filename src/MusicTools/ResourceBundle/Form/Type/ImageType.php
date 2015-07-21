5eeab3313e5bd1106bb1d093162bc9c8194c9685
Elorfin <penin.axel@gmail.com>
2015-07-21 09:39:46 +0200

    fix code styles

--- src/MusicTools/ResourceBundle/Form/Type/ImageType.php
+++ src/MusicTools/ResourceBundle/Form/Type/ImageType.php
@@ -4,8 +4,6 @@ namespace MusicTools\ResourceBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\FormEvent;
-use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ImageType extends AbstractType

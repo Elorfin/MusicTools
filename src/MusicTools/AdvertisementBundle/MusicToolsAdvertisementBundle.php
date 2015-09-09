a1d2a5d681136e186ec11980079324488d02ae19
Elorfin <penin.axel@gmail.com>
2015-09-09 08:18:06 +0200

    initialize social bundles

---
+++ src/MusicTools/AdvertisementBundle/MusicToolsAdvertisementBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace MusicTools\AdvertisementBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MusicToolsAdvertisementBundle extends Bundle
+{
+}
---
+++ src/MusicTools/ForumBundle/MusicToolsForumBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace MusicTools\ForumBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MusicToolsForumBundle extends Bundle
+{
+}

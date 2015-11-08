e16f52f8c99ed8b9a81d3b32c59b1c1cfb596c9f
Elorfin <penin.axel@gmail.com>
2015-11-08 14:03:26 +0100

    new filter to access resource path

---
+++ web/client/app/Utilities/Filter/ResourcePathFilter.js
@@ -0,0 +1,14 @@
+/**
+ * Resource Path filter
+ */
+angular
+    .module('Utilities')
+    .filter('resource_path', [
+        'ApiService',
+        function (ApiService) {
+            return function (path) {
+                return ApiService.getServer() + '/' + path;
+            };
+        }
+    ]
+);

8e9a23ab74d8f343fcb103beb2d4e2bf2ebe5151
Elorfin <penin.axel@gmail.com>
2015-11-11 09:49:32 +0100

    update translations

--- web/client/app/SongBook/translations.js
+++ web/client/app/SongBook/translations.js
@@ -8,12 +8,16 @@ var songBookTranslations = {};
  * Language = EN
  */
 songBookTranslations['en'] = {
+    song: 'song{COUNT, plural, =0{} one{} other{s}}',
     no_song_found: 'No song found.'
 };
+
+
 
 /**
  * Language = FR
  */
 songBookTranslations['fr'] = {
+    song: 'morceau{PLURAL, select, true{x}}',
     no_song_found: 'Aucun morceau trouvé.'
 };

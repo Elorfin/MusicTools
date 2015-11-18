59c2c7c57025688dedfab203d739b0fd68c66f05
Elorfin <penin.axel@gmail.com>
2015-11-18 22:38:29 +0100

    add Utilities in top of compiled file for inheritance

--- web/client/Gruntfile.js
+++ web/client/Gruntfile.js
@@ -69,6 +69,7 @@ module.exports = function (grunt) {
                     'app/app.js',
 
                     // Load src code
+                    'app/Utilities/**/*.js',
                     'app/**/*.js',
 
                     // Load routes

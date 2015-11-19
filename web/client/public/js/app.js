2da6d91ef5d5061820b027972efa0c690943a0b9
Elorfin <penin.axel@gmail.com>
2015-11-19 08:27:52 +0100

    update compiled files

--- web/client/public/js/app.js
+++ web/client/public/js/app.js
@@ -2624,6 +2624,26 @@ angular
  * Created by Corum on 18/11/15.
  */
 
+// File : app/Theory/Directive/Interval/IntervalPlayerDirective.js
+angular
+    .module('Theory')
+    .directive('intervalPlayer', [
+        function IntervalPlayerDirective() {
+            return {
+                restrict: 'E',
+                templateUrl: '../app/Theory/Partial/Interval/player.html',
+                replace: true,
+                scope: {
+                },
+                controller: function IntervalPlayerController() {},
+                controllerAs: 'intervalPlayerCtrl',
+                bindToController: true,
+                link: function (scope, element, attrs) {
+
+                }
+            };
+        }
+    ]);
 // File : app/Theory/Directive/Note/NoteDisplaySwitchDirective.js
 angular
     .module('Theory')

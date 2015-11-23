c95f9a7e9660b82192f821f8de577d889d6f815e
Elorfin <penin.axel@gmail.com>
2015-11-23 15:50:16 +0100

    active item in sidebar

--- web/client/app/Layout/Directive/Sidebar/SidebarItemDirective.js
+++ web/client/app/Layout/Directive/Sidebar/SidebarItemDirective.js
@@ -1,26 +1,30 @@
 /**
  * Represents a link in the sidebar
  */
+var LayoutSidebarItemDirective = function LayoutSidebarItemDirectiveConstructor($location) {
+    return {
+        restrict: 'E',
+        templateUrl: '../app/Layout/Partial/Sidebar/sidebar-item.html',
+        replace: true,
+        scope: {
+            icon       : '@',
+            label      : '@',
+            url        : '@',
+            currentPath: '='
+        },
+        link: function sidebarItemLink(scope, element, attrs) {
+            // Watch current path changes
+            scope.$watch('currentPath', function () {
+                scope.current = (scope.currentPath && 0 === scope.currentPath.indexOf(scope.url));
+            });
+        }
+    };
+};
+
+// Set up dependency injection
+LayoutSidebarItemDirective.$inject = [ '$location' ];
+
+// Register into AngularJS
 angular
     .module('Layout')
-    .directive('layoutSidebarItem', [
-        '$route',
-        function LayoutSidebarItemDirective($route) {
-            return {
-                restrict: 'E',
-                templateUrl: '../app/Layout/Partial/Sidebar/sidebar-item.html',
-                replace: true,
-                scope: {
-                    icon  : '@',
-                    label : '@',
-                    url   : '@'
-                },
-                link: function (scope, element, attrs) {
-                    scope.current = false;
-                    if ($route.current && $route.current.regexp) {
-                        scope.current = $route.current.regexp.test(scope.url);
-                    }
-                }
-            };
-        }
-    ]);
+    .directive('layoutSidebarItem', LayoutSidebarItemDirective);

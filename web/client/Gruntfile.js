71c2370c461daf312cbb406f0e703360b18939ef
Elorfin <penin.axel@gmail.com>
2016-02-09 11:42:45 +0100

    organize styles

--- web/client/Gruntfile.js
+++ web/client/Gruntfile.js
@@ -206,7 +206,7 @@ module.exports = function (grunt) {
 
             less: {
                 files: [
-                    'less/**/*.less'
+                    'styles/**/*.less'
                 ],
                 tasks: [ 'less' ]
             }

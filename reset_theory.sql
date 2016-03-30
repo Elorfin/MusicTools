8d9023fdeb17c10a86a4076a3588be5f496d76a6
Elorfin <penin.axel@gmail.com>
2016-03-30 13:44:51 +0200

    archive

--- reset_theory.sql
+++
@@ -1,9 +0,0 @@
-SET foreign_key_checks = 0;
-
-TRUNCATE theory_note;
-TRUNCATE theory_note_info;
-TRUNCATE theory_interval;
-TRUNCATE theory_degree;
-TRUNCATE theory_scale;
-TRUNCATE theory_chord;
-TRUNCATE theory_chord_interval;

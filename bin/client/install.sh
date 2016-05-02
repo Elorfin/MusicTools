9333d603ba3f455f1cc4e2661855eef8d09e48fe
Elorfin <penin.axel@gmail.com>
2016-05-02 10:50:23 +0200

    add install script for client

--- bin/client/install.sh
+++ bin/client/install.sh
@@ -1,9 +1,28 @@
 #!/bin/sh
 
-API_PATH='client'
+SRC_PATH='client'
 CURRENT_DIR="$(dirname "$0")"
 
 # Get colors
 source $CURRENT_DIR/../shared/colors.sh
 
 echo -e "${BLUE}[Client installation]${NC}"
+
+echo "Move into API directory ($SRC_PATH)"
+cd $SRC_PATH
+echo "Done"
+
+# Get dependencies
+echo "Install dependencies..."
+npm install
+bower install
+echo "Done"
+
+# Build project
+echo "Build Angular 1 web application"
+grunt build
+echo "Done"
+
+echo "Build Angular 2 web application"
+npm run webpack
+echo "Done"

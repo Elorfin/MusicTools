b2418e6fdf97c562c597658430b11885bfd4e05f
Elorfin <penin.axel@gmail.com>
2016-06-23 10:09:03 +0200

    implement update script for client

--- bin/client/update.sh
+++ bin/client/update.sh
@@ -1,1 +1,23 @@
 #!/bin/sh
+
+SRC_PATH='client'
+CURRENT_DIR="$(dirname "$0")"
+
+# Get colors
+source $CURRENT_DIR/../shared/colors.sh
+
+echo -e "${BLUE}[Client update]${NC}"
+
+echo "Move into Client directory ($SRC_PATH)"
+cd $SRC_PATH
+echo "Done"
+
+# Get dependencies
+echo "Update dependencies..."
+npm update
+echo "Done"
+
+# Build project (compile TypeScripts + dump assets)
+echo "Build Angular web application"
+npm run build
+echo "Done"

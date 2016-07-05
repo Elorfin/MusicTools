f1f1ee5917c789713e407c5c07d05241def6b3c9
Elorfin <penin.axel@gmail.com>
2016-07-05 08:21:58 +0200

    tests with dynamic components

--- client/src/components/instrument/detail.component.ts
+++ client/src/components/instrument/detail.component.ts
@@ -1,28 +1,44 @@
-import {Component}                from '@angular/core';
+import {Component, ViewContainerRef, ReflectiveInjector}                from '@angular/core';
 import {NgIf}                     from '@angular/common';
 import {Template}                 from '../../library/layout/template.service';
 import {Instrument}               from './instrument';
 import {InstrumentService}        from "./instrument.service";
 import {RouteSegment, OnActivate} from '@angular/router';
+import {SpecificationFactory} from "../instrument-specification/factory";
 
 @Component({
     selector: 'instrument-detail',
     templateUrl: Template.getUrl('detail.component.html', 'instrument'),
-    directives: [NgIf]
+    directives: [NgIf],
+    providers: [SpecificationFactory]
 })
 
 export class InstrumentDetailComponent implements OnActivate {
     public instrument: Instrument;
     public errorMessage: any;
 
-    constructor (private instrumentService: InstrumentService) {}
+    constructor (
+        private vcRef: ViewContainerRef,
+        private instrumentService: InstrumentService,
+        private specificationFactory: SpecificationFactory) {}
 
     routerOnActivate(current: RouteSegment) {
         return this.instrumentService
             .get(current.getParam('id'))
             .subscribe(
-                instrument => this.instrument = instrument,
+                instrument => this.loadInstrument(instrument),
                 error      => this.errorMessage = <any>error
             );
+    }
+
+    loadInstrument(instrument: Instrument) {
+        this.instrument = instrument
+
+        /*const instrumentType = this.instrument.relationships.instrumentType;*/
+
+        this.specificationFactory.createComponent('').then(factory => {
+            const injector = ReflectiveInjector.fromResolvedProviders([], this.vcRef.parentInjector);
+            this.vcRef.createComponent(factory, 0, injector, []);
+        });
     }
 }

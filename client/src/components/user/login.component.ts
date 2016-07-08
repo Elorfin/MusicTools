8cf35607ee17e2bc8e9d5775b5c76f57ec83385e
Elorfin <penin.axel@gmail.com>
2016-07-08 08:39:02 +0200

    [Client] initializes authentication

---
+++ client/src/components/user/login.component.ts
@@ -0,0 +1,33 @@
+import {Component} from '@angular/core';
+import {Router} from '@angular/router';
+import {FORM_DIRECTIVES, FormBuilder, Validators, ControlGroup, NgIf} from '@angular/common'
+import {Template}  from '../../library/layout/template.service';
+import {AuthenticationService}  from '../../library/security/authentication.service';
+
+@Component({
+    selector: 'login',
+    templateUrl: Template.getUrl('login.component.html', 'user'),
+    directives:  [ FORM_DIRECTIVES, NgIf ]
+})
+
+/**
+ * Login
+ */
+export class LoginComponent {
+    form: ControlGroup;
+    error: boolean = false;
+    constructor(fb: FormBuilder, public auth: AuthenticationService, public router: Router) {
+        this.form = fb.group({
+            username:  ['', Validators.required],
+            password:  ['', Validators.required]
+        });
+    }
+
+    onSubmit(value: any) {
+        this.auth.login(value.username, value.password)
+            .subscribe(
+                (token: any) => this.router.navigate(['../Home']),
+                () => { this.error = true; }
+            );
+    }
+}

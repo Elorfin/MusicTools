0e39d5f15fa95e0687b2af819c0aa5267fb25c05
Elorfin <penin.axel@gmail.com>
2016-07-15 08:25:50 +0200

    [Client] cleans commented code

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,18 +1,16 @@
 // Angular dependencies
 import {HTTP_PROVIDERS, RequestOptions} from '@angular/http';
 import {provide}                        from '@angular/core';
-/*import {ROUTER_PROVIDERS}               from '@angular/router';*/
 import {bootstrap}                      from '@angular/platform-browser-dynamic';
 
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/toPromise';
-/*import 'rxjs/add/operator/of';*/
 
 // Load root component
-import {AppComponent}      from './components/app.component';
-import {APP_ROUTER_PROVIDERS}      from './components/app.routes';
-import {ApiRequestOptions} from "./library/api/api-request-options";
+import {AppComponent}         from './components/app.component';
+import {APP_ROUTER_PROVIDERS} from './components/app.routes';
+import {ApiRequestOptions}    from "./library/api/api-request-options";
 
 bootstrap(AppComponent, [
     HTTP_PROVIDERS,
